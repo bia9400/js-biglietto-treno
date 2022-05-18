06a6564da99e7f29f29737de6c184ffcd777d113
bia9400 <ferro.biagio@gmail.com>
2022-05-18 14:18:36 +0200

    fine

---
+++ app.js
@@ -0,0 +1,66 @@
+
+
+
+
+
+
+
+let eta = prompt("Qual è la tua età?");
+let km = prompt("Quanti chilometri devi percorrere?");
+let prezzoBiglietto;
+let tariffaAlKilometro = km * 0.21;
+let etaLenght = eta.length
+let kmLenght = km.length
+
+
+if (isNaN(eta) && isNaN(km)) {
+    alert("Inserire dei valori numerici nei campi Età e Chilometri")
+}
+else if (isNaN(eta) && !isNaN(km)) {
+    alert("Inserire un Età valida (es:8..98)")
+}
+else if (!isNaN(eta) && isNaN(km)) {
+    alert("Inserire un chilometraggio minimo (es: 0...1520)")
+}
+else {
+    if (etaLenght < 0 || kmLenght < 0) {
+        alert("Inserire valori numerici positivi dentro i campi Età e Km")
+    }
+    else if (km > 0 && km < 10) {
+        alert("La tua tratta non esiste, inserire un chilometraggio più grande")
+    }
+    else if (km > 2000) {
+        alert("Seee vabbéé siamo Trenitalia mica Starlink..Inserisci un chilometraggio più corto")
+    }
+    else if (eta > 0 && eta < 5) {
+        alert("Il biglietto per i minori di 6 anni è cmpletamente gratuito")
+        let printTicket = document.getElementById("exercise")
+        printTicket.innerHTML = (`<h1>Per Età: ${eta} anni costo biglietto: 0$ </h1> `)
+    }
+    else {
+        if (eta < 18) {
+            prezzoBiglietto = tariffaAlKilometro - (tariffaAlKilometro * 0.2)
+            let printTicket = document.getElementById("exercise")
+
+            printTicket.innerHTML = (`<li>Età: ${eta} anni </li> <li>Chilometri da percorrere: ${km} Km</li> <li>Prezzo biglietto: ${(Math.round(prezzoBiglietto*100)/100)} $</li>`)
+        }
+        else if (eta > 65) {
+            prezzoBiglietto = tariffaAlKilometro - (tariffaAlKilometro * 0.4)
+            let printTicket = document.getElementById("exercise")
+
+            printTicket.innerHTML = (`<li>Età: ${eta} anni </li> <li>Chilometri da percorrere: ${km} Km</li> <li>Prezzo biglietto: ${(Math.round(prezzoBiglietto*100)/100)} $</li>`)
+        }
+        else {
+            prezzoBiglietto = tariffaAlKilometro
+            let printTicket = document.getElementById("exercise")
+
+            printTicket.innerHTML = (`<li>Età: ${eta} anni </li> <li>Chilometri da percorrere: ${km} Km</li> <li>Prezzo biglietto: ${(Math.round(prezzoBiglietto*100)/100)} $</li>`)
+            
+        }
+    }
+
+}
+
+
+
+
